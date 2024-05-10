f4ca06c9086928245f4196262dfd958d58904839
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-05-09 23:16:58 -0600

    Create Edad.java

---
+++ Edad.java
@@ -0,0 +1,12 @@
+public class Edad {
+    public static void main(String[] args) {
+        double anios = 19;
+        double dias = 365;
+        double horas = 24;
+        double min = 60;
+        double seg = 60;
+        System.out.println( anios*dias*horas*min*seg);
+
+    }
+    
+}
